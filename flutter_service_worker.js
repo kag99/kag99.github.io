3e6639bc5fa077eb74bcd4d2aa5becf2fca68e44
kag99 <kag99@rediffmail.com>
2023-04-19 18:52:59 +0530

    Added service invocaton

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8d2c6310d0f7c1d4ff4a3fd3a94c0be2",
-"index.html": "7d0267a012ee9698dba3956238bd00e4",
-"/": "7d0267a012ee9698dba3956238bd00e4",
-"main.dart.js": "6aa69e27b8e69328ea9e343633f07257",
+"index.html": "f10b5bba11ecd599b30bdf088bd981c3",
+"/": "f10b5bba11ecd599b30bdf088bd981c3",
+"main.dart.js": "6287be5e32702523195504310bf989a8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "99e9ebbdeb6479522426b33d796a7645",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "b48bd9be818bbadf257dada15252c7d8",
+"assets/NOTICES": "7bbaa7d8557d47f4d52d94bb26bfd6df",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
